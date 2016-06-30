8bd4abc0daeeb83786c58f939caf8f16d87a38c2
XBigTK13X <xbigtk13x@gmail.com>
2016-06-30 12:07:33 -0500

    Allow SSL site loading for news

--- launcher/src/launcher/util/BrowserComponent.java
+++ launcher/src/launcher/util/BrowserComponent.java
@@ -4,16 +4,23 @@ package launcher.util;
 import launcher.util.DesktopApi;
 import launcher.util.LaunchLogger;
 import org.apache.commons.io.FileUtils;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.xhtmlrenderer.simple.XHTMLPanel;
 import org.xhtmlrenderer.swing.FSMouseListener;
 import org.xhtmlrenderer.swing.LinkListener;
 
+import javax.net.ssl.*;
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 import java.net.URI;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 public class BrowserComponent {
     private static class HyperlinkHandler extends LinkListener {
@@ -58,14 +65,40 @@ public class BrowserComponent {
         _news.addMouseTrackingListener(new HyperlinkHandler());
     }
 
+    // Taken from http://nanashi07.blogspot.tw/2014/06/enable-ssl-connection-for-jsoup.html
+    public static void enableSSLSocket() throws KeyManagementException, NoSuchAlgorithmException {
+        HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
+
+        SSLContext context = SSLContext.getInstance("TLS");
+        context.init(null, new X509TrustManager[]{new X509TrustManager() {
+            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            }
+
+            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            }
+
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+        }}, new SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
+    }
+
     public void load(final String url) {
         try {
+            enableSSLSocket();
             //TODO Figure out a way to do this without writing to a temp file
 
             //The process here is (read HTML) -> (write HTML to file) -> (read file with flying-saucer) -> (f-s render)
             //There MUST be a simpler way to use flying-saucer, but I've spent ALL day trying out other web
             //components for swing. This is working (better than JEditorPane), so it is staying for now.
-            Document doc = Jsoup.connect(url).get();
+            Connection con = Jsoup.connect(url);
+            con.ignoreHttpErrors(true);
+            Document doc = con.get();
             String contents = doc.toString();
             File tmp = new File("news.html");
             FileUtils.write(tmp, contents);
@@ -75,6 +108,7 @@ public class BrowserComponent {
         }
         catch (Exception e) {
             LaunchLogger.error("The latest news could not be loaded.");
+            LaunchLogger.error(e.getMessage());
         }
     }
 }
